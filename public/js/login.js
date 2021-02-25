1c42efee4b5ade1f6c1ad9952170738bea634194
JohnG2055 <51770537+JohnG2055@users.noreply.github.com>
2021-02-25 01:14:40 -0800

    json and login

---
+++ public/js/login.js
@@ -0,0 +1,50 @@
+// // Call this function when the page loads (the "ready" event)
+// $(document).ready(function() {
+// 	initializePage();
+// })
+
+// /*
+//  * Function that is called when the document is ready.
+//  */
+// function initializePage() {
+// 	// $('#login-btn').click(addProjectDetails);
+
+//     $( "form" ).submit(function( event ) {
+//         console.log( $( this ).serializeArray() );
+//         event.preventDefault();
+//       });
+// 	// $('#colorBtn').click(randomizeColors);
+// }
+
+// /*
+//  * Make an AJAX call to retrieve project details and add it in
+//  */
+// // function addProjectDetails(e) {
+// // 	// Prevent following the link
+// // 	e.preventDefault();
+
+// //     var element = document.getElementById("user-email");
+
+// //     console.log(element)
+
+
+// // 	// // Get the div ID, e.g., "project3"
+// // 	// var projectID = $(this).closest('.project').attr('id');
+// // 	// // get rid of 'project' from the front of the id 'project3'
+// // 	// var idNumber = projectID.substr('project'.length);
+
+// // 	// console.log("User clicked on project " + idNumber);
+// // 	// $.get("https://cogs120-fb.herokuapp.com/project/" + idNumber, callBackFn);
+
+// // }
+
+// // $( "form" ).submit(function( event ) {
+// //     console.log( $( this ).serializeArray() );
+// //     event.preventDefault();
+// //   });
+
+
+
+// function createAccount() {
+  
+// }
--- routes/activity.js
+++ routes/activity.js
@@ -1,3 +1,11 @@
-exports.view = function(req, res){
-    res.render('activity');
+// exports.view = function(req, res){
+//     res.render('activity');
+// };
+
+var data = require('../activity.json');
+
+
+exports.view = function(request, response){
+	console.log(data);
+	response.render('activity', data);
 };
